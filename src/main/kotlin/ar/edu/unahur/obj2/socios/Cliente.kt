101b0a969f8649c975dad4e8de40144b65037a14
Damian Takashima <d.takas23@gmail.com>
2021-05-09 21:32:37 -0300

    primer commit

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -1,4 +1,23 @@
 package ar.edu.unahur.obj2.socios
 
-class Cliente {
+class pedido(var cliente: Cliente, var monto: Float) {
+    fun propina() = cliente.propinaQueDeja(monto)
 }
+
+
+class Cliente(var estadoDeAnimo: EstadoDeAnimo, var barrio: Barrio) {
+    fun propinaQueDeja(monto: Float) {
+
+    }
+}
+
+
+abstract class EstadoDeAnimo() {
+
+}
+
+
+abstract class Barrio() {
+
+}
+
