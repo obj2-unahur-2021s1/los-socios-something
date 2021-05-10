a74b00f8006c210e5e8ef336ba177555c1907c1c
Damian Takashima <d.takas23@gmail.com>
2021-05-10 14:41:57 -0300

    primer commit

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -1,23 +1,43 @@
 package ar.edu.unahur.obj2.socios
 
-class pedido(var cliente: Cliente, var monto: Float) {
-    fun propina() = cliente.propinaQueDeja(monto)
+class pedido(var cliente: Cliente, var montoPedido: Float) {
+    fun propina() = cliente.propinaQueDeja(montoPedido)
 }
 
 
-class Cliente(var estadoDeAnimo: EstadoDeAnimo, var barrio: Barrio) {
-    fun propinaQueDeja(monto: Float) {
-
+class Cliente(var estadoDeAnimo: EstadoDeAnimo, var barrio: Barrio, var dineroEnBolsillo: Float) {
+    fun propinaQueDeja(montoPedido: Float) {
+        return estadoDeAnimo.propinaPorAnimo(montoPedido) + barrio.propinaPorBarrio()
     }
 }
 
 
-abstract class EstadoDeAnimo() {
 
+
+abstract class EstadoDeAnimo() {
+    abstract fun propinaPorAnimo(montoPedido: Float)
 }
 
+object Enojado : EstadoDeAnimo() {
+    override fun propinaPorAnimo(montoPedido: Float) = 0
+}
+object Feliz : EstadoDeAnimo() {
+    override fun propinaPorAnimo(montoPedido: Float) = montoPedido * 0.25
+}
+object Indiferente : EstadoDeAnimo() {
+    override fun propinaPorAnimo(montoPedido: Float) = dineroEnBolsillo
+}
+object Resfriado : EstadoDeAnimo() {
+    override fun propinaPorAnimo(montoPedido: Float) = montoPedido
+}
 
 abstract class Barrio() {
-
+    abstract fun propinaPorBarrio()
 }
 
+object LasRosas : Barrio() {
+    override fun propinaPorBarrio() = 50
+}
+object LasLauchas : Barrio() {
+    override fun propinaPorBarrio() = 
+}
