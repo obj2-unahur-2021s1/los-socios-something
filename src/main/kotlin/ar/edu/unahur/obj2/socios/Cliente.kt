791a02e707784b5ba5a8cef32c7a1e1dc4187508
VallesFranco <vallesfrancodavid@gmail.com>
2021-05-11 11:40:43 -0300

    Ejercicio completo

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -1,44 +1,51 @@
 package ar.edu.unahur.obj2.socios
 
-import kotlin.math.min
+import kotlin.math.max
 
 class Pedido(val cliente: Cliente, val montoPedido: Float) {
-    fun propina() = cliente.propinaQueDeja(montoPedido)
+    fun propina() = cliente.calcularPropina(montoPedido)
 }
 
 class Cliente(var estadoDeAnimo: EstadoDeAnimo, var barrio: Barrio, var dineroEnBolsillo: Float) {
-    fun propinaQueDeja(montoPedido: Float) : Float {
-        return barrio.propinaPorBarrio(estadoDeAnimo)
+    fun calcularPropina(montoPedido: Float) : Float {
+        return barrio.propinaPorBarrio(estadoDeAnimo, montoPedido, dineroEnBolsillo)
     }
 }
 
 abstract class EstadoDeAnimo() {
-    abstract fun propinaPorAnimo(montoPedido: Float) : Float
+    abstract fun propinaPorAnimo(montoPedido: Float, dineroEnBolsillo: Float) : Float
 }
 
 object Enojado : EstadoDeAnimo() {
-    override fun propinaPorAnimo(montoPedido: Float) = 0f
+    override fun propinaPorAnimo(montoPedido: Float, dineroEnBolsillo: Float) = 0f
 }
 object Feliz : EstadoDeAnimo() {
-    override fun propinaPorAnimo(montoPedido: Float) = (montoPedido * 0.25).toFloat()
+    override fun propinaPorAnimo(montoPedido: Float, dineroEnBolsillo: Float) = (montoPedido * 0.25).toFloat()
 }
 object Indiferente : EstadoDeAnimo() {
-    override fun propinaPorAnimo(montoPedido: Float) = dineroEnBolsillo
+    override fun propinaPorAnimo(montoPedido: Float, dineroEnBolsillo: Float) = dineroEnBolsillo
 }
 object Resfriado : EstadoDeAnimo() {
-    override fun propinaPorAnimo(montoPedido: Float) = montoPedido
+    override fun propinaPorAnimo(montoPedido: Float, dineroEnBolsillo: Float) = montoPedido
 }
 
 abstract class Barrio() {
-    abstract fun propinaPorBarrio(estadoDeAnimo: EstadoDeAnimo) : Float
+    abstract fun propinaPorBarrio(estadoDeAnimo: EstadoDeAnimo, montoPedido: Float, dineroEnBolsillo: Float) : Float
 }
 
 object LasRosas : Barrio() {
-    override fun propinaPorBarrio(estadoDeAnimo: EstadoDeAnimo) = estadoDeAnimo.propinaPorAnimo(montoPedido) + 50
+    override fun propinaPorBarrio(estadoDeAnimo: EstadoDeAnimo, montoPedido: Float, dineroEnBolsillo: Float) =
+        estadoDeAnimo.propinaPorAnimo(montoPedido, dineroEnBolsillo) + 50f
 }
 object LasLauchas : Barrio() {
-    override fun propinaPorBarrio(estadoDeAnimo: EstadoDeAnimo) = estadoDeAnimo.propinaPorAnimo(montoPedido) / 2
+    override fun propinaPorBarrio(estadoDeAnimo: EstadoDeAnimo, montoPedido: Float, dineroEnBolsillo: Float) =
+        estadoDeAnimo.propinaPorAnimo(montoPedido, dineroEnBolsillo) / 2
 }
 object BarrioVerde : Barrio() {
-    override fun propinaPorBarrio(estadoDeAnimo: EstadoDeAnimo) = min(estadoDeAnimo.propinaPorAnimo(montoPedido), 200)
+    override fun propinaPorBarrio(estadoDeAnimo: EstadoDeAnimo, montoPedido: Float, dineroEnBolsillo: Float) =
+        max(200f, estadoDeAnimo.propinaPorAnimo(montoPedido, dineroEnBolsillo))
+}
+object LasTorres : Barrio() {
+    override fun propinaPorBarrio(estadoDeAnimo: EstadoDeAnimo, montoPedido: Float, dineroEnBolsillo: Float) =
+        estadoDeAnimo.propinaPorAnimo(montoPedido, dineroEnBolsillo)
 }
--- src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
@@ -4,13 +4,43 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
 class ClienteTest : DescribeSpec({
-  describe("Un/a cliente") {
+  describe("Un/a cliente y un pedido") {
     val cliente1 = Cliente(Feliz, LasRosas, 600f)
     val pedido1 = Pedido(cliente1, 1000f)
 
-    describe("propina feliz en las rosas") {
-      it("25% + 50 de 600 = 200"){
-        pedido1.propina().shouldBe(200)
+    describe("Propina feliz en las rosas") {
+      it("25% de 1000 + 50 = 300"){
+        pedido1.propina().shouldBe(300f)
+      }
+    }
+  }
+  describe("Un/a cliente y un pedido v2") {
+    val cliente2 = Cliente(Indiferente, LasLauchas, 500f)
+    val pedido2 = Pedido(cliente2, 150f)
+
+    describe("Propina indiferente en las lauchas") {
+      it("500 / 2 = 250"){
+        pedido2.propina().shouldBe(250f)
+      }
+    }
+  }
+  describe("Un/a cliente y un pedido v3") {
+    val cliente3 = Cliente(Enojado, BarrioVerde, 400f)
+    val pedido3 = Pedido(cliente3, 1500f)
+
+    describe("Propina enojado en barrio verde") {
+      it("Mínimo 200 de 0 = 200"){
+        pedido3.propina().shouldBe(200f)
+      }
+    }
+  }
+  describe("Un/a cliente y un pedido v4") {
+    val cliente4 = Cliente(Resfriado, LasTorres, 300f)
+    val pedido4 = Pedido(cliente4, 500f)
+
+    describe("Propina resfriado en las torres") {
+      it("100% de 500 = 500"){
+        pedido4.propina().shouldBe(500f)
       }
     }
   }
